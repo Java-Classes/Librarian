a8db10d84374b0c85a1b50c3a028d647af62ae51
Paul <paulageyev@gmail.com>
2018-02-13 11:49:53 +0200

    Create 'produceEvent' method for 'UpdateBookCommandTest'

--- api-java/src/test/java/io/spine/javaclasses/exlibris/c/aggregate/definition/UpdateBookCommandTest.java
+++ api-java/src/test/java/io/spine/javaclasses/exlibris/c/aggregate/definition/UpdateBookCommandTest.java
@@ -1,23 +1,37 @@
+/*
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package io.spine.javaclasses.exlibris.c.aggregate.definition;
 
+import com.google.protobuf.Message;
 import io.spine.javaclasses.exlibris.testdata.BookCommandFactory;
-import io.spine.net.EmailAddress;
-import io.spine.net.Url;
-import io.spine.people.PersonName;
-import javaclasses.exlibris.AuthorName;
 import javaclasses.exlibris.Book;
-import javaclasses.exlibris.BookDetails;
 import javaclasses.exlibris.BookDetailsChange;
-import javaclasses.exlibris.BookId;
-import javaclasses.exlibris.BookSynopsis;
-import javaclasses.exlibris.BookTitle;
-import javaclasses.exlibris.Category;
-import javaclasses.exlibris.UserId;
 import javaclasses.exlibris.c.AddBook;
+import javaclasses.exlibris.c.BookUpdated;
 import javaclasses.exlibris.c.UpdateBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.spine.javaclasses.exlibris.testdata.BookCommandFactory.bookDetails;
 import static io.spine.javaclasses.exlibris.testdata.BookCommandFactory.bookDetails2;
@@ -25,6 +39,7 @@ import static io.spine.javaclasses.exlibris.testdata.BookCommandFactory.createBo
 import static io.spine.javaclasses.exlibris.testdata.BookCommandFactory.updateBookInstance;
 import static io.spine.server.aggregate.AggregateMessageDispatcher.dispatchCommand;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Paul Ageyev
@@ -36,6 +51,40 @@ public class UpdateBookCommandTest extends BookCommandTest<UpdateBook> {
     @BeforeEach
     public void setUp() {
         super.setUp();
+    }
+
+    @Test
+    @DisplayName("produce BookUpdated event")
+    void produceEvent() {
+
+        dispatchAddBookCmd();
+
+        final BookDetailsChange bookDetailsChange = BookDetailsChange.newBuilder()
+                                                                     .setPreviousBookDetails(
+                                                                             bookDetails)
+                                                                     .setNewBookDetails(
+                                                                             bookDetails2)
+                                                                     .build();
+
+        final UpdateBook updateBook = updateBookInstance(BookCommandFactory.bookId,
+                                                         BookCommandFactory.userId,
+                                                         bookDetailsChange);
+
+        final List<? extends Message> messageList = dispatchCommand(aggregate,
+                                                                    envelopeOf(updateBook));
+
+        assertNotNull(aggregate.getId());
+        assertEquals(1, messageList.size());
+        assertEquals(BookUpdated.class, messageList.get(0)
+                                                   .getClass());
+
+        final BookUpdated bookUpdated = (BookUpdated) messageList.get(0);
+
+        assertEquals(BookCommandFactory.bookId, bookUpdated.getBookId());
+
+        assertEquals(bookDetails2.getTitle(), bookUpdated.getBookDetailsChange()
+                                                         .getNewBookDetails()
+                                                         .getTitle());
     }
 
     @Test
