24627ac066bd30af502e918042d336783478bd43
alexander.karpetc <alexander.karpetc@gmail.com>
2018-02-16 15:43:45 +0200

    Merge master

--- api-java/src/test/java/io/spine/javaclasses/exlibris/c/aggregate/definition/ReturnBookCommandTest.java
+++ api-java/src/test/java/io/spine/javaclasses/exlibris/c/aggregate/definition/ReturnBookCommandTest.java
@@ -42,6 +42,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
