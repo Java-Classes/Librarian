46c28b93fcbb1ab9a6ca7a3dafcca66563ce51f4
alexander.karpetc <alexander.karpetc@gmail.com>
2018-02-20 13:36:09 +0200

    Add event subscriber and rejection subscriber for `AddBook` command, tests for these subscribers

---
+++ api-java/src/test/java/javaclasses/exlibris/c/integrational/SubscribersTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package javaclasses.exlibris.c.integrational;
+
+import io.spine.core.Ack;
+import io.spine.core.Command;
+import io.spine.grpc.MemoizingObserver;
+import io.spine.grpc.StreamObservers;
+import io.spine.server.BoundedContext;
+import io.spine.server.commandbus.CommandBus;
+import io.spine.server.event.EventBus;
+import io.spine.server.rejection.RejectionBus;
+import javaclasses.exlibris.c.AddBook;
+import javaclasses.exlibris.c.BookAdded;
+import javaclasses.exlibris.c.aggregate.BookCommandTest;
+import javaclasses.exlibris.c.rejection.Rejections;
+import javaclasses.exlibris.context.BoundedContexts;
+import javaclasses.exlibris.testdata.BookCommandFactory;
+import javaclasses.exlibris.testdata.BookEventSubscriber;
+import javaclasses.exlibris.testdata.BookRejectionsSubscriber;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.spine.protobuf.TypeConverter.toMessage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class SubscribersTest extends BookCommandTest<AddBook> {
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    @DisplayName("produce book already exists rejection")
+    void produceRejection() {
+        final Command addBook = requestFactory.command()
+                                              .create(toMessage(
+                                                      BookCommandFactory.createBookInstance()));
+        final BoundedContext boundedContext = BoundedContexts.create();
+        final CommandBus commandBus = boundedContext.getCommandBus();
+        final MemoizingObserver<Ack> memorizingObserver = StreamObservers.memoizingObserver();
+        commandBus.post(addBook, StreamObservers.noOpObserver());
+        commandBus.post(addBook, memorizingObserver);
+//        AnyPacker.unpack(memorizingObserver.responses());
+        assertEquals(" ", memorizingObserver.firstResponse());
+    }
+
+    @Test
+    @DisplayName("throw BookAlreadyExists rejection")
+    void throwMenuNotAvailable() {
+
+        final BoundedContext boundedContext = BoundedContexts.create();
+        final CommandBus commandBus = boundedContext.getCommandBus();
+        final RejectionBus rejectionBus = boundedContext.getRejectionBus();
+
+        final Command addBook = requestFactory.command()
+                                              .create(toMessage(
+                                                      BookCommandFactory.createBookInstance()));
+
+        final BookRejectionsSubscriber vendorRejectionsSubscriber = new BookRejectionsSubscriber();
+
+        rejectionBus.register(vendorRejectionsSubscriber);
+
+        assertNull(BookRejectionsSubscriber.getRejection());
+
+        commandBus.post(addBook, StreamObservers.noOpObserver());
+        commandBus.post(addBook, StreamObservers.noOpObserver());
+
+        Rejections.BookAlreadyExists menuNotAvailable = BookRejectionsSubscriber.getRejection();
+
+        assertEquals("paulageyev@gmail.com", menuNotAvailable.getLibrarianId()
+                                                             .getEmail()
+                                                             .getValue());
+        assertEquals("0201485672", menuNotAvailable.getBookId()
+                                                   .getIsbn62()
+                                                   .getValue());
+        assertEquals("Refactoring", menuNotAvailable.getBookTitle()
+                                                    .getTitle());
+    }
+
+    @Test
+    @DisplayName("catch event")
+    void catchEvent() {
+
+        final BoundedContext boundedContext = BoundedContexts.create();
+        final CommandBus commandBus = boundedContext.getCommandBus();
+        final EventBus eventBus = boundedContext.getEventBus();
+        final Command addBook = requestFactory.command()
+                                              .create(toMessage(
+                                                      BookCommandFactory.createBookInstance()));
+
+        final BookEventSubscriber eventSubscriber = new BookEventSubscriber();
+        eventBus.register(eventSubscriber);
+        commandBus.post(addBook, StreamObservers.noOpObserver());
+        final BookAdded event = BookEventSubscriber.getEvent();
+        assertEquals("0201485672", event.getBookId()
+                                        .getIsbn62()
+                                        .getValue());
+        assertEquals("Refactoring", event.getDetails()
+                                         .getTitle()
+                                         .getTitle());
+        assertEquals("paulageyev@gmail.com", event.getLibrarianId()
+                                                  .getEmail()
+                                                  .getValue());
+    }
+}
