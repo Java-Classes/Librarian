526378b2f0b87d331c9710189237a3b7388575a0
yegor.udovchenko <yegor.udovchenko@teamdev.com>
2018-05-03 14:31:13 +0300

    Complete LoansExtensionProcman tests using flow use cases.

--- api-java/src/test/java/javaclasses/exlibris/c/procman/ReservationQueueProcmanTest.java
+++ api-java/src/test/java/javaclasses/exlibris/c/procman/ReservationQueueProcmanTest.java
@@ -20,101 +20,20 @@
 
 package javaclasses.exlibris.c.procman;
 
-import com.google.protobuf.Message;
-import io.grpc.stub.StreamObserver;
-import io.spine.client.TestActorRequestFactory;
-import io.spine.core.Ack;
-import io.spine.core.Command;
-import io.spine.grpc.StreamObservers;
-import io.spine.server.BoundedContext;
-import io.spine.server.commandbus.CommandBus;
-import javaclasses.exlibris.BoundedContexts;
-import javaclasses.exlibris.c.inventory.InventoryCommandTest;
-import javaclasses.exlibris.c.inventory.InventoryRepository;
-import javaclasses.exlibris.testdata.BookRejectionsSubscriber;
-import javaclasses.exlibris.testdata.InventoryRejectionsSubscriber;
+import javaclasses.exlibris.Reservation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.spine.protobuf.TypeConverter.toMessage;
-import static javaclasses.exlibris.testdata.BookCommandFactory.createBookInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.appendInventoryInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.borrowBookInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.cancelReservationInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.inventoryId;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.inventoryItemId;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.markReservationExpiredInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.reserveBookInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.returnBookInstance;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.userId2;
-import static javaclasses.exlibris.testdata.InventoryCommandFactory.userId3;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("ReservationQueueProcman should")
-public class ReservationQueueProcmanTest extends InventoryCommandTest<Message> {
-    private final TestActorRequestFactory requestFactory =
-            TestActorRequestFactory.newInstance(getClass());
-
-    private final Command addBook = requestFactory.createCommand(toMessage(createBookInstance()));
-
-    private final Command appendInventoryItem1 =
-            requestFactory.createCommand(toMessage(appendInventoryInstance()));
-
-    private final Command borrowBookUser1Item1 =
-            requestFactory.createCommand(toMessage(borrowBookInstance()));
-
-    private final Command borrowBookUser2Item1 =
-            requestFactory.createCommand(toMessage(borrowBookInstance(inventoryId,
-                                                                      inventoryItemId,
-                                                                      userId2)));
-
-    private final Command borrowBookUser3Item1 =
-            requestFactory.createCommand(toMessage(borrowBookInstance(inventoryId,
-                                                                      inventoryItemId,
-                                                                      userId3)));
-
-    private final Command returnBookUser1Item1 =
-            requestFactory.createCommand(toMessage(returnBookInstance()));
-
-    private final Command reserveBookUser2 =
-            requestFactory.createCommand(toMessage(reserveBookInstance(userId2,
-                                                                       inventoryId)));
-
-    private final Command reserveBookUser3 =
-            requestFactory.createCommand(toMessage(reserveBookInstance(userId3,
-                                                                       inventoryId)));
-
-    private final Command cancelReservationUser2 =
-            requestFactory.createCommand(toMessage(cancelReservationInstance(inventoryId,
-                                                                             userId2)));
-
-    private final Command expireReservationUser3 =
-            requestFactory.createCommand(toMessage(markReservationExpiredInstance(inventoryId,
-                                                                                  userId3)));
-
-    private BoundedContext boundedContext;
-    private CommandBus commandBus;
-    private StreamObserver<Ack> observer;
-    private BookRejectionsSubscriber bookRejectionsSubscriber;
-    private InventoryRejectionsSubscriber inventoryRejectionsSubscriber;
-
+public class ReservationQueueProcmanTest extends ProcessManagerTest {
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp();
-        InventoryRepository.setNewInstance();
-        boundedContext = BoundedContexts.create();
-        commandBus = boundedContext.getCommandBus();
-        observer = StreamObservers.noOpObserver();
-        bookRejectionsSubscriber = new BookRejectionsSubscriber();
-        inventoryRejectionsSubscriber = new InventoryRejectionsSubscriber();
-        boundedContext.getRejectionBus()
-                      .register(bookRejectionsSubscriber);
-        boundedContext.getRejectionBus()
-                      .register(inventoryRejectionsSubscriber);
-        commandBus.post(addBook, observer);
     }
 
     @Test
@@ -137,6 +56,8 @@ public class ReservationQueueProcmanTest extends InventoryCommandTest<Message> {
         commandBus.post(returnBookUser1Item1, observer);
         commandBus.post(borrowBookUser1Item1, observer);
 
+        final Reservation reservation = getAggregateState().getReservations(0);
+        assertTrue(reservation.getIsSatisfied());
         assertTrue(inventoryRejectionsSubscriber.wasCalled());
     }
 
